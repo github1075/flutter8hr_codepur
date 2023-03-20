39dd7aa5b2bb2e5eb9000d2aa890aa0df2e3d9f3
github1075 <siammtalha8506@gmail.com>
2023-03-21 00:22:30 +0600

    lear to add dart file lear routes,theme textformfeild,google font,add image,plugin from pub dev,elevatedbutton,column singlechildscrollview,navigator to navigate from one page to another,use static name for routes in util folder

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:demo/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,52 +8,54 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login2.png", fit: BoxFit.cover,),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize:20,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login2.png", fit: BoxFit.cover,),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize:20,
 
+              ),
             ),
-          ),
-          SizedBox(height:16,),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical:16,horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration:InputDecoration(
-                    labelText: "username",
-                    hintText: "enter your name"
+            SizedBox(height:16,),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:16,horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration:InputDecoration(
+                      labelText: "username",
+                      hintText: "enter your name"
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "password",
-                    hintText: "enter your password",
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "password",
+                      hintText: "enter your password",
 
+                    ),
                   ),
-                ),
-                SizedBox(height:32,),
-                ElevatedButton(
-                     style:TextButton.styleFrom(),
-                    onPressed:(){
-                       print("hi siam");
+                  SizedBox(height:32,),
+                  ElevatedButton(
+                       style:TextButton.styleFrom(maximumSize: Size(170,50)),
+                      onPressed:(){
+                         Navigator.pushNamed(context,MyRoutes.homeRoute);
 
-                    },
-                    child:Text(
-                      "submit",
+                      },
+                      child:Text(
+                        "submit",
 
-                    )
-                )
-              ],
-            ),
-          )
-        ],
+                      )
+                  )
+                ],
+              ),
+            )
+          ],
 
+        ),
       ),
       
     );
