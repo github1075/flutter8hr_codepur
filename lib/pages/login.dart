abe711f73584fd86d0dbfb3af6127d3fb227c841
github1075 <siammtalha8506@gmail.com>
2023-03-22 19:15:37 +0600

    lear to add dart file lear routes,theme textformfeild,google font,add image,plugin from pub dev,elevatedbutton,column singlechildscrollview,navigator to navigate from one page to another,use static name for routes in util folder animatedcontainer,inkwell,animation,

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,16 @@
 import 'package:demo/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name=" ";
+  bool clickSubmit=false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -13,7 +20,7 @@ class LoginPage extends StatelessWidget {
           children: [
             Image.asset("assets/images/login2.png", fit: BoxFit.cover,),
             Text(
-              "Welcome",
+              "Welcome $name ",
               style: TextStyle(
                 fontSize:20,
 
@@ -29,6 +36,12 @@ class LoginPage extends StatelessWidget {
                       labelText: "username",
                       hintText: "enter your name"
                     ),
+                    onChanged: (value){
+                      name=value;
+                      setState(() {
+
+                      });
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -39,16 +52,47 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height:32,),
-                  ElevatedButton(
-                       style:TextButton.styleFrom(maximumSize: Size(170,50)),
-                      onPressed:(){
-                         Navigator.pushNamed(context,MyRoutes.homeRoute);
+                  // ElevatedButton(
+                  //      style:TextButton.styleFrom(maximumSize: Size(170,50)),
+                  //     onPressed:(){
+                  //        Navigator.pushNamed(context,MyRoutes.homeRoute);
+                  //
+                  //     },
+                  //     child:Text(
+                  //       "submit",
+                  //
+                  //     )
+                  // )
+                  InkWell(
+                    onTap: ()async{
+                      clickSubmit=true;
+                      await Future.delayed(Duration(seconds:2));
+                      Navigator.pushNamed(context,MyRoutes.homeRoute);
+                      setState(() {
 
-                      },
-                      child:Text(
+                      });
+                    },
+                    
+                    child: AnimatedContainer(
+                      duration:Duration(seconds:1),
+                      height:50,
+                      width:clickSubmit?90:120,
+                      alignment:Alignment.center,
+                      child:clickSubmit?
+                          Icon(Icons.done,color: Colors.white,)
+                          : Text(
                         "submit",
+                        style:TextStyle(
+                          fontSize:25,
+                          color:Colors.white
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(8),
 
-                      )
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -57,7 +101,7 @@ class LoginPage extends StatelessWidget {
 
         ),
       ),
-      
+
     );
   }
 }
